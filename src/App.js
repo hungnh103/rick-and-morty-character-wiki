d42efc205515e250684e4e5dfc54872af8af42f5
Nguyen Huy Hung <hungnh103@gmail.com>
2022-10-22 18:31:06 +0700

    build the search bar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ function App() {
   let [fetchedData, updateFetchedData] = useState([]);
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState('');
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
   useEffect(() => {
     (async function() {
       let data = await fetch(api).then((res) => res.json());
@@ -26,15 +29,17 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+
       <div className="container">
-      <div className="row">
-        Filter component will be placed here
-        <div className="col-lg-8 col-12">
-          <div className="row">
-            <Card results={results} />
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
