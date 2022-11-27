39466608f1ef04fb50a917a49422a347ccd6c71b
Nguyen Huy Hung <hungnh103@gmail.com>
2022-11-27 09:49:55 +0700

    set up react router

--- src/App.js
+++ src/App.js
@@ -8,10 +8,33 @@ import Pagination from './components/Pagination/Pagination';
 import Filter from './components/Filter/Filter';
 import Navbar from './components/Navbar/Navbar';
 
+import Episodes from './Pages/Episodes';
+import Location from './Pages/Location';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/*<Route path='/:id' element={<CardDetails />} />*/}
+
+        <Route path='/episodes' element={<Episodes />} />
+        {/*<Route path='/episodes/:id' element={<CardDetails />} />*/}
+        <Route path='/location' element={<Location />} />
+        {/*<Route path='/location/:id' element={<CardDetails />} />*/}
+      </Routes>
+    </Router>
+  );
+}
+
+const Home = () => {
   let [fetchedData, updateFetchedData] = useState([]);
   let { info, results } = fetchedData;
 
